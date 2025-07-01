ce27e378306b8c770ded8001a6f32d8e682c0bdd
Omkar-203 <omkaradsare2003@gmail.com>
2025-07-01 15:30:21 +0530

    make changed

--- front-end/src/components/signup.js
+++ front-end/src/components/signup.js
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {  // Renamed component to start with an uppercase letter
     const [name, setName] = useState("");
